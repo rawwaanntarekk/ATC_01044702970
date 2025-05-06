3f2073eab6c58166cd36d1c3f00571c39bade3a5
Rawan Tarek <115898539+rawwaanntarekk@users.noreply.github.com>
2025-05-06 07:35:59 +0300

    Add Event repository.

---
+++ Areeb.DAL/Repositories/Implementations/EventRepository.cs
@@ -0,0 +1,42 @@
+﻿using Areeb.DAL.Entities;
+using Areeb.DAL.Repositories.Interfaces;
+using Event_Booking_System.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Areeb.DAL.Repositories.Implementations
+{
+    internal class EventRepository(ApplicationDbContext applicationDbContext) : IEventRepository
+    {
+        private readonly ApplicationDbContext _applicationDbContext = applicationDbContext;
+
+        public async Task<IEnumerable<Event>> GetAllEventsAsync() => await _applicationDbContext.Events.ToListAsync();
+
+        public async Task<Event> GetEventByIdAsync(int id)
+        {
+            var eventEntity = await _applicationDbContext.Events.FirstOrDefaultAsync(e => e.Id == id);
+            return eventEntity ?? throw new Exception($"Event with ID {id} not found.");
+        }
+
+        public async Task AddEventAsync(Event applicationEvent)
+        {
+              await _applicationDbContext.Events.AddAsync(applicationEvent);
+              await _applicationDbContext.SaveChangesAsync();
+        }
+        public  void UpdateEventAsync(Event applicationEvent)
+        {
+            var existingEvent = _applicationDbContext.Events.FirstOrDefault(e => e.Id == applicationEvent.Id) ?? throw new Exception($"Event with ID {applicationEvent.Id} not found.");
+            _applicationDbContext.Update(applicationEvent);
+        }
+
+        public void DeleteEventAsync(int id)
+        {
+            var eventEntity = _applicationDbContext.Events.FirstOrDefault(e => e.Id == id) ?? throw new Exception($"Event with ID {id} not found.");
+            _applicationDbContext.Events.Remove(eventEntity);
+        }
+
+       
+
+        
+
+    }
+}
