c1def1a0dc0fb0b15fa1dbae5fdd8469f6d89738
Rawan Tarek <115898539+rawwaanntarekk@users.noreply.github.com>
2025-05-08 11:21:39 +0300

    feat: create an event booking.

--- Areeb.DAL/Repositories/Implementations/EventRepository.cs
+++ Areeb.DAL/Repositories/Implementations/EventRepository.cs
@@ -5,30 +5,30 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Areeb.DAL.Repositories.Implementations
 {
-    public class EventRepository(ApplicationDbContext applicationDbContext) : IEventRepository
+    public class EventRepository(ApplicationDbContext applicationDbContext) : IGenericRepository<Event>
     {
         private readonly ApplicationDbContext _applicationDbContext = applicationDbContext;
 
-        public async Task<IEnumerable<Event>> GetAllEventsAsync() => await _applicationDbContext.Events.Include(e => e.Bookings).ToListAsync();
+        public async Task<IEnumerable<Event>> GetAllAsync() => await _applicationDbContext.Events.Include(e => e.Bookings).ToListAsync();
 
-        public async Task<Event> GetEventByIdAsync(int id)
+        public async Task<Event> GetByIdAsync(int id)
         {
             var eventEntity = await _applicationDbContext.Events.Include(e => e.Bookings).FirstOrDefaultAsync(e => e.Id == id);
             return eventEntity ?? throw new Exception($"Event with ID {id} not found.");
         }
 
-        public async Task AddEventAsync(Event applicationEvent)
+        public async Task AddAsync(Event applicationEvent)
         {
               await _applicationDbContext.Events.AddAsync(applicationEvent);
               await _applicationDbContext.SaveChangesAsync();
         }
-        public  void UpdateEventAsync(Event applicationEvent)
+        public  void Update(Event applicationEvent)
         {
             var existingEvent = _applicationDbContext.Events.FirstOrDefault(e => e.Id == applicationEvent.Id) ?? throw new Exception($"Event with ID {applicationEvent.Id} not found.");
             _applicationDbContext.Update(applicationEvent);
         }
 
-        public void DeleteEventAsync(int id)
+        public void Delete(int id)
         {
             var eventEntity = _applicationDbContext.Events.FirstOrDefault(e => e.Id == id) ?? throw new Exception($"Event with ID {id} not found.");
             _applicationDbContext.Events.Remove(eventEntity);
---
+++ Event Booking System/Models/CreateBookingViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Event_Booking_System.Models
+{
+    public class CreateBookingViewModel
+    {
+        [Required]
+        [Range(1, int.MaxValue, ErrorMessage = "Please choose a valid quantity.")]
+        public int Quantity { get; set; }
+        public int EventId { get; set; }
+        public string EventName { get; set; }
+    }
+}
