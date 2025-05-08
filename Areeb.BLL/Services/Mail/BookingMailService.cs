5f0e9903ab4645689c17ad6fea2d221ce59553ae
Rawan Tarek <115898539+rawwaanntarekk@users.noreply.github.com>
2025-05-08 14:15:10 +0300

    feat: Send a confirmation mail after booking an event.

---
+++ Areeb.BLL/Services/Mail/BookingMailService.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Areeb.BLL.Services.Mail
+{
+    public class BookingMailService : MailService
+    {
+        public BookingMailService(IConfiguration configuration) : base(configuration){}
+
+        public async Task SendBookingConfirmationAsync(string to, string eventName, string eventDate)
+        {
+            var templateService = new EmailTemplateService();
+
+            string template = templateService.LoadTemplate("BookConfirmationTemplate");
+
+            var userName = to.Split('@')[0];
+
+            var placeholders = new Dictionary<string, string>
+            {
+                { "UserName", userName },
+                { "EventName", eventName },
+                { "EventDate", eventDate }
+            };
+
+            string body = templateService.ReplacePlaceholders(template, placeholders);
+            string subject = "Your Booking Confirmation";
+
+            await SendEmailAsync(to, subject, body);
+
+        }
+
+    }
+}
---
+++ Areeb.BLL/Services/Mail/MailService.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.Configuration;
+using MimeKit;
+using MimeKit.Text;
+using MailKit.Net.Smtp;
+using MailKit.Security;
+
+
+namespace Areeb.BLL.Services.Mail
+{
+    public abstract class MailService : IMailService
+    {
+        private readonly IConfiguration _configuration;
+
+        public MailService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public async Task SendEmailAsync(string to, string subject, string body)
+        {
+            var email = new MimeMessage();
+            email.From.Add(MailboxAddress.Parse(_configuration["MailSettings:MailUser"]));
+            email.To.Add(MailboxAddress.Parse(to));
+            email.Subject = subject;
+            email.Body = new TextPart(TextFormat.Html)
+            {
+                Text = body
+            };
+
+           using var smtp = new SmtpClient();
+           await smtp.ConnectAsync(_configuration["MailSettings:MailHost"], int.Parse(_configuration["MailSettings:MailPort"]), SecureSocketOptions.StartTls);
+           await smtp.AuthenticateAsync(_configuration["MailSettings:MailUser"], _configuration["MailSettings:MailPassword"]);
+           await smtp.SendAsync(email);
+           await smtp.DisconnectAsync(true);
+        }
+
+
+    }
+}
