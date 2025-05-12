7b7593fb12c37b86339593a50c3a6f2dff6e7404
Rawan Tarek <115898539+rawwaanntarekk@users.noreply.github.com>
2025-05-12 14:52:03 +0300

    feat: add event image upload for Admin and display in Event view.

---
+++ Areeb.BLL/Services/EventService/IEventService.cs
@@ -0,0 +1,22 @@
+﻿using Areeb.BLL.DTOs;
+using Areeb.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Areeb.BLL.Services.EventService
+{
+    public interface IEventService
+    {
+        Task<IEnumerable<Event>> GetAllEventsAsync();
+        Task<Event> GetEventByIdAsync(int id);
+
+        Task<IEnumerable<Category>> GetCategoriesAsync();
+        Task CreateEventAsync(CreatedEventDTO @event);
+
+        void UpdateEvent(Event @event);
+        void DeleteEvent(int id);
+    }
+}
--- Areeb.DAL/Data/Migrations/20250512091916_Add Category and imageUrl to event migration.Designer.cs
+++ Areeb.DAL/Data/Migrations/20250512091916_Add Category and imageUrl to event migration.Designer.cs
@@ -4,6 +4,7 @@ using Event_Booking_System.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Event_Booking_System.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250512091916_Add Category and imageUrl to event migration")]
+    partial class AddCategoryandimageUrltoeventmigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -64,8 +67,8 @@ namespace Event_Booking_System.Data.Migrations
                     b.Property<int>("Capacity")
                         .HasColumnType("int");
 
-                    b.Property<byte>("Category")
-                        .HasColumnType("tinyint");
+                    b.Property<int>("Category")
+                        .HasColumnType("int");
 
                     b.Property<DateTime>("CreatedAt")
                         .ValueGeneratedOnAdd()
